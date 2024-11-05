838f270539c1f82961ae00b95e696a623a8249f2
J <juanma.lorenzo@correo.ucu.edu.uy>
2024-11-05 00:37:17 -0300

    Correcion de errores en entorno a los pokemons, ahora utilizan un metodo para enviar la informacion y poder crear las clases, probalmente cambiarlo por un sistema de visitor

--- src/Library/FabricaPokemon.cs
+++ src/Library/FabricaPokemon.cs
@@ -1,10 +1,13 @@
 namespace ClassLibrary;
 
 public static class FabricaPokemon
-{   
+{   /// <summary>
+    /// Los registros son clases que tienenl la informacion de la creacion de cada pokemon. Estos tienen un metodo que
+    /// instancia los pokemons como objetos y los envia hacia las listas, se deberia hacer con visitor.
+    /// </summary>
     public  static Dictionary<int, IRegistroPokemon> PokedexPokemon = new Dictionary<int, IRegistroPokemon >();
 
-    public static void CargarPokemons()
+    public static void CargarPokemons()// Para entender mejor leer IRegistro y Registro. 
     {
         PokedexPokemon.Add(1, new Registro("Bulbasur", 3, 3,4));
         PokedexPokemon.Add(2, new Registro("ABC", 333, 333,3));
@@ -12,13 +15,13 @@ public static class FabricaPokemon
             
     }
     public static void InstanciarPokes(List<int> entrada, IPlayer Jugador) // Tiene que llegarle los valores del player.
-    {
+    {// Falta traer la info desde jugador hacia aca. 
         List<IPokemon> PokemonsTemporal  = new List<IPokemon>();
         foreach (int numero in entrada)
         {
-            PokemonsTemporal.Add(PokedexPokemon[numero].CrearPokemon());
+            PokemonsTemporal.Add(PokedexPokemon[numero].CrearPokemon()); // Son muchos puntos probablemente aplicar visitor
         }
-        Jugador.EstablecerEquipo(PokemonsTemporal);
+        Jugador.EstablecerEquipo(PokemonsTemporal); 
         
     }
 }
