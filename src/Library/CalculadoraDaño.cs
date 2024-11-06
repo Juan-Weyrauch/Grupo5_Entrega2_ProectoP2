0d2e1f625b181377412cce71732590e8d31e8ee5
Santiago Pellejero <santiago.pellejero@correo.ucu.edu.uy>
2024-11-06 20:43:10 -0300

    Arreglos a la calculadora?
    # Conflicts:
    #	src/Library/Static/TablaDeTipos.cs

--- src/Library/CalculadoraDaño.cs
+++ src/Library/CalculadoraDaño.cs
@@ -4,11 +4,10 @@ namespace Library;
 
 public class CalculadoraDaño
 {
-    static double CalcularEfectividad(string tipoAtaque, string tipo1, string tipo2)
+    static double CalcularEfectividad(string tipoAtaque, string tipo1)
     {
         double multiplicador = 1.0;
 
-        // Verificar si el ataque es fuerte contra el primer tipo
         if (TablaDeTipos[tipoAtaque].fortalezas.Contains(tipo1))
         {
             multiplicador *= 2.0;
@@ -25,25 +24,7 @@ public class CalculadoraDaño
         {
             multiplicador *= 1.0;
         }
-
-        // Verificar si el ataque es fuerte contra el segundo tipo
-        if (TablaDeTipos[tipoAtaque].fortalezas.Contains(tipo2))
-        {
-            multiplicador *= 2.0;
-        }
-        else if (TablaDeTipos[tipoAtaque].debilidades.Contains(tipo2))
-        {
-            multiplicador *= 0.5;
-        }
-        else if (TablaDeTipos[tipoAtaque].inmunidades.Contains(tipo1))
-        {
-            multiplicador *= 0.0;
-        }
-        else
-        {
-            multiplicador *= 1.0;
-        }
-
+        
         return multiplicador;
     }
 }
