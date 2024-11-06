a6aa6f4091a72519264c58faa18bcccf70369911
Jesus Dos Santos <jesus.dossantos@correo.ucu.edu.uy>
2024-11-05 23:20:59 -0300

    funcionalidad items

--- src/Library/Player.cs
+++ src/Library/Player.cs
@@ -5,6 +5,27 @@ public class Player : IPlayer
     public string Name { get; }
     // inventario quizas? Aca? 
     public List <IPokemon> Equipo { get;  set; }
+    
+    // Agrega el inventario de ítems
+    public List<IItem> Inventario { get; private set; }
+    
+    public Player(string name)
+    {
+        Name = name;
+        Equipo = new List<IPokemon>();
+            
+        // Inicializar el inventario con los ítems requeridos
+        Inventario = new List<IItem>
+        {
+            new SuperPotion(),
+            new SuperPotion(),
+            new SuperPotion(),
+            new SuperPotion(),
+            new Revive(),
+            new FullRestore(),
+            new FullRestore()
+        };
+    }
     public void Elegir(List<int> eleccionplayer)
     {
         // Visitor a FabricaPokemon
