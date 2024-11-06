64725d1ac7a9a37de6003faf7a71f9603c526fb1
J <juanma.lorenzo@correo.ucu.edu.uy>
2024-11-06 19:56:00 -0300

    Creacion de la clase ataque

---
+++ src/Library/Ataque.cs
@@ -0,0 +1,9 @@
+namespace Library;
+
+public class Ataque
+{
+    string Name { get; }
+    int Poder { get;  }
+    int Precsion { get;  }
+    string Tipo { get; }
+}
---
+++ src/Library/Interfaces/IAtaque.cs
@@ -0,0 +1,10 @@
+namespace ClassLibrary;
+
+public class IAtaque
+{
+    string Name { get; }
+    int Poder { get;  }
+    int Precsion { get;  }
+    string Tipo { get; }
+    
+} 
