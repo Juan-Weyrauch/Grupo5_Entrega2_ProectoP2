601621aaf7c5f4e167eeda21677e110623bd0d5c
J <juanma.lorenzo@correo.ucu.edu.uy>
2024-11-08 23:35:22 -0300

    cambios en fachada para que funcione de  manera correcta

--- src/Library/Static/Fachada.cs
+++ src/Library/Static/Fachada.cs
@@ -1,19 +1,23 @@
 
 
+using ClassLibrary;
+
 namespace ClassLibrary;
 
-    public static class Fachada
+public static class Fachada
+{
+    public static void Start()
     {
-        public static void Start()
-    { // maybe this should return an int and just evalueate that 
-      // meaning that i just call Printer, Printer calls Calculator which returns a value to Printer
-      // and I just evaluate that number here
+        // maybe this should return an int and just evalueate that 
+        // meaning that i just call Printer, Printer calls Calculator which returns a value to Printer
+        // and I just evaluate that number here
         while (true)
         {
             ImpresoraDeTexto.startPrint();
             int inicial = Convert.ToInt32(Console.ReadLine());
             if (inicial == 1)
             {
+                FabricaPokemon.CargarPokemons();
                 Fachada.selecciones();
             }
             else if (inicial == 2)
@@ -28,7 +32,7 @@ namespace ClassLibrary;
                 Console.ReadKey(); // Esperar a que el usuario presione una tecla antes de continuar
                 continue;
             }
-            
+
         }
     }
 
@@ -36,52 +40,55 @@ namespace ClassLibrary;
     /// 
     /// </summary>
     public static void selecciones()
+    {   
+        List<IPlayer> players = new List<IPlayer>();
+        players.Add(CrearJugador(1));
+        players.Add(CrearJugador(2));
+    }
+
+    public static IPlayer CrearJugador(int numero)
     {
-        
         string inputName = "";
         List<int> valuesForPokemons = new List<int>();
-        List<IPlayer> players = new List<IPlayer>(); // maybe it's better to send a list index
-        
+
+
         //para poder llamar a 'GetValidatedNumber(1, n)'
         int n = FabricaPokemon.DevolverTotal(); // debe ser la cantidad de pokemons que mostremos en la matriz
 
-        for (int g = 1; g <= 2; g++)
+        Console.WriteLine($"Enter the name of Player {numero}:");
+        inputName = Console.ReadLine();
+
+        // if (g == 1)
         {
-            Console.WriteLine($"Enter the name of Player {g}:");
-            inputName = Console.ReadLine();
-            
-           // if (g == 1)
+            ImpresoraDeTexto.mostrarListaPokemons(inputName);
+            for (int j = 0; j < 6; j++)
             {
-                ImpresoraDeTexto.mostrarListaPokemons(inputName);
-                for (int j = 0; j < 6; j++)
+                ImpresoraDeTexto.selectYourPokemon();
+                try
                 {
-                    ImpresoraDeTexto.selectYourPokemon();
-                    try
-                    {
-                        // este metodo recibe y verifica el valor ingresado
-                        int numberOfPokemonSelected = Convert.ToInt32(Console.ReadLine());
-                       // numberOfPokemonSelected = Calculator.GetValidatedNumber(1, n, numberOfPokemonSelected);
-                        valuesForPokemons.Add(numberOfPokemonSelected);
-                    }
-                    catch (FormatException)
-                    {
-                        Console.WriteLine("Invalid format. Please enter a valid integer.");
-                        g--;
-                    }
-                    catch (ArgumentOutOfRangeException)
-                    {
-                        Console.WriteLine("Number out of range. Please enter a number within the allowed range.");
-                        g--;
-                    }
+                    // este metodo recibe y verifica el valor ingresado
+                    int numberOfPokemonSelected = Convert.ToInt32(Console.ReadLine());
+                    // numberOfPokemonSelected = Calculator.GetValidatedNumber(1, n, numberOfPokemonSelected);
+                    valuesForPokemons.Add(numberOfPokemonSelected);
                 }
-
-                List < IPokemon> PokemonsCreados = FabricaPokemon.InstanciarPokes(valuesForPokemons);
-                ImpresoraDeTexto.EligePokemonInicial(PokemonsCreados);
-                Console.WriteLine("Elige el pokemon inicial// no es definitivo.");
-                int eleccion = Convert.ToInt32(Console.ReadLine());
-                players.Add(new Player(inputName, PokemonsCreados, /*TODO revise this attribute*/ eleccion));
-
+                catch (FormatException)
+                {
+                    Console.WriteLine("Invalid format. Please enter a valid integer.");
+                    j--;
+                }
+                catch (ArgumentOutOfRangeException)
+                {
+                    Console.WriteLine("Number out of range. Please enter a number within the allowed range.");
+                    j--;
+                }
             }
+
+            List<IPokemon> PokemonsCreados = FabricaPokemon.InstanciarPokes(valuesForPokemons);
+            ImpresoraDeTexto.EligePokemonInicial(PokemonsCreados);
+            Console.WriteLine("Elige el pokemon inicial// no es definitivo.");
+            int eleccion = Convert.ToInt32(Console.ReadLine());
+            IPlayer player = new Player(inputName, PokemonsCreados, /*TODO revise this attribute*/ eleccion);
+            return player;
         }
     }
 }
