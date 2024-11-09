a16750e8b58c73c0bea3d94e7856aa50082b412d
J <juanma.lorenzo@correo.ucu.edu.uy>
2024-11-09 00:05:58 -0300

    cambios en ataques

--- src/Library/Static/FabricaAtaque.cs
+++ src/Library/Static/FabricaAtaque.cs
@@ -7,21 +7,21 @@ using System.Collections.Generic;
 
 public static class FabricaAtaque
 {
+    static Dictionary<int, IAtaque> ataquesDiccionario = new Dictionary<int, IAtaque>();
+
     // Metodo estatico para ejecutar y agregar ataques al diccionario
     public static void Executar()
     {
-        Dictionary<int, IAtaque> ataquesDiccionario = new Dictionary<int, IAtaque>();
 
         // Lista de algunos ataques populares de Pokemon con nombres y tipos en español (formato: nombre, poder, precisión, tipo, causaEfectoEspecial)
         List<(string, int, int, string, int)> ataquesPokemon = new List<(string, int, int, string, int)>
         {
             ("Placaje", 40, 100, "Normal", 0),
             ("Latigo Cepa", 45, 100, "Planta", 0),
-            ("Lanzallamas", 90, 100, "Fuego", 1),  // Puede causar quemadura
-            ("Rayo", 90, 100, "Electrico", 3),  // Puede paralizar
+            ("Lanzallamas", 90, 100, "Fuego", 1), // Puede causar quemadura
+            ("Rayo", 90, 100, "Electrico", 3), // Puede paralizar
             ("Pistola de Agua", 40, 100, "Agua", 0),
             ("Terremoto", 100, 100, "Tierra", 0),
-            ("Bola Sombra", 80, 100, "Fantasma", 0),
             ("Hiperrayo", 150, 90, "Normal", 0),
             ("Psiquico", 90, 100, "Psiquico", 0),
             ("Rayo Hielo", 90, 100, "Hielo", 0),
@@ -29,9 +29,9 @@ public static class FabricaAtaque
             ("Surf", 90, 100, "Agua", 0),
             ("Garra Dragon", 80, 100, "Dragon", 0),
             ("Cola de Hierro", 100, 75, "Acero", 0),
-            ("Tormenta de Hielo", 110, 70, "Hielo", 0),  
-            ("Bomba Lodo", 90, 100, "Veneno", 2),  // Puede envenenar
-            ("Trueno", 110, 70, "Electrico", 3),  // Puede paralizar
+            ("Tormenta de Hielo", 110, 70, "Hielo", 0),
+            ("Bomba Lodo", 90, 100, "Veneno", 2), // Puede envenenar
+            ("Trueno", 110, 70, "Electrico", 3), // Puede paralizar
             ("Rayo Solar", 120, 100, "Planta", 0),
             ("Cargallamas", 50, 100, "Fuego", 0),
             ("Cola Aqua", 90, 90, "Agua", 0),
@@ -44,17 +44,17 @@ public static class FabricaAtaque
             ("Patada Baja", 60, 100, "Lucha", 0),
             ("Esfera Aura", 80, 100, "Lucha", 0),
             ("Golpes Furiosos", 18, 80, "Normal", 0),
-            ("Zumbido", 90, 100, "Bicho", 4),  // Puede dormir
+            ("Zumbido", 90, 100, "Bicho", 4), // Puede dormir
             ("Hoja Afilada", 55, 95, "Planta", 0),
-            ("Puño Hielo", 75, 100, "Hielo", 0),  
+            ("Puño Hielo", 75, 100, "Hielo", 0),
             ("Puño Fuego", 75, 100, "Fuego", 0),
-            ("Puño Trueno", 75, 100, "Electrico", 3),  // Puede paralizar
+            ("Puño Trueno", 75, 100, "Electrico", 3), // Puede paralizar
             ("Tormenta Floral", 130, 90, "Planta", 0),
             ("Carga Salvaje", 90, 100, "Electrico", 0),
             ("Impacto Gigante", 150, 90, "Normal", 0),
             ("Combate Cercano", 120, 100, "Lucha", 0),
             ("Cabezazo Zen", 80, 90, "Psiquico", 0),
-            ("Puya Nociva", 80, 100, "Veneno", 2),  // Puede envenenar
+            ("Puya Nociva", 80, 100, "Veneno", 2), // Puede envenenar
             ("Erupcion de Vapor", 110, 95, "Agua", 0),
             ("Pulso Draco", 85, 100, "Dragon", 0),
             ("Bomba Destrozadora", 80, 100, "Hielo", 0),
@@ -94,14 +94,43 @@ public static class FabricaAtaque
             ("Tormenta Hoja", 130, 90, "Planta", 0),
             ("Colmillo Psiquico", 85, 100, "Psiquico", 0),
             ("Terror Nocturno", 85, 95, "Oscuro", 0),
-            ("Disparo Toxico", 120, 80, "Veneno", 2)  // Puede envenenar
+            ("Disparo Toxico", 120, 80, "Veneno", 2) // Puede envenenar
         };
 
         // Agregamos los ataques al diccionario
-        for (int i = 0; i < ataquesPokemon.Count; i++)
+        for (int i = 1; i < ataquesPokemon.Count; i++)
         {
             var ataque = ataquesPokemon[i];
             ataquesDiccionario.Add(i, new Ataque(ataque.Item1, ataque.Item2, ataque.Item3, ataque.Item4, ataque.Item5));
         }
+
     }
 }
+/*
+
+public static List<IAtaque> GenerarAtaquesRandom()
+    {
+        Random rand = new Random(); // Inicializa el generador de números aleatorios
+        List<IAtaque> ataquesSeleccionados = new List<IAtaque>(); // Lista donde guardaremos los ataques seleccionados
+
+        // Obtener todas las claves del diccionario (los identificadores de los ataques)
+        List<int> claves = new List<int>(ataquesDiccionario.Keys);
+
+        // Seleccionar 4 ataques aleatorios
+        for (int i = 0; i < 4; i++)
+        {
+            // Seleccionar una clave aleatoria del diccionario
+            int claveAleatoria = claves[rand.Next(claves.Count)];
+
+            // Obtener el ataque correspondiente a esa clave
+            IAtaque ataque = ataquesDiccionario[claveAleatoria];
+
+
+            // Agregar el ataque a la lista de ataques seleccionados
+            ataquesSeleccionados.Add(ataque);
+        }
+
+        return ataquesSeleccionados;
+    }
+}
+*/
