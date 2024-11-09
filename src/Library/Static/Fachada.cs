5fe5dccd1cb4ded1ef31d6a5b4878e45e262eb32
Juan Ignacio Weyrauch <juan.weyrauch@correo.ucu.edu.uy>
2024-11-09 00:28:58 -0300

    refactor de Fachada, mantiene funcionalidad

--- src/Library/Static/Fachada.cs
+++ src/Library/Static/Fachada.cs
@@ -1,5 +1,3 @@
-
-
 using ClassLibrary;
 
 namespace ClassLibrary;
@@ -8,73 +6,73 @@ public static class Fachada
 {
     public static void Start()
     {
-        // maybe this should return an int and just evalueate that 
-        // meaning that i just call Printer, Printer calls Calculator which returns a value to Printer
-        // and I just evaluate that number here
         while (true)
         {
             ImpresoraDeTexto.startPrint();
-            int inicial = Convert.ToInt32(Console.ReadLine());
-            if (inicial == 1)
+
+            if (int.TryParse(Console.ReadLine(), out int inicial)) // si no es valido el input no se ejecutara (else)
             {
-                FabricaPokemon.CargarPokemons();
-                Fachada.selecciones();
-            }
-            else if (inicial == 2)
-            {
-                ImpresoraDeTexto.endPrint();
-                break;
+                if (inicial == 1)
+                {
+                    FabricaPokemon.CargarPokemons();
+                    selecciones();
+                }
+                else if (inicial == 2)
+                {
+                    ImpresoraDeTexto.endPrint();
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Please enter a valid number (1 or 2). Press any key to try again...");
+                    Console.ReadKey();
+                }
             }
             else
             {
-                Console.WriteLine("Por favor, introduce un número válido (1 o 2).");
-                Console.WriteLine("Presiona cualquier tecla para intentar de nuevo...");
-                Console.ReadKey(); // Esperar a que el usuario presione una tecla antes de continuar
-                continue;
+                Console.WriteLine("Invalid input. Please enter 1 or 2.");
             }
-
         }
+    }
+
+    /// <summary>
+    /// selecciones crea una lista de dos jugadores
+    /// </summary>
+    public static void selecciones()
+    {
+        List<IPlayer> players = new List<IPlayer>
+        {
+            CrearJugador(1),
+            CrearJugador(2)
+        };
     }
 
     /// <summary>
     /// 
     /// </summary>
-    public static void selecciones()
-    {   
-        List<IPlayer> players = new List<IPlayer>();
-        players.Add(CrearJugador(1));
-        players.Add(CrearJugador(2));
-    }
-
-    public static IPlayer CrearJugador(int numero)
+    /// <param name="numero"></param>
+    /// <returns></returns>
+    public static Player CrearJugador(int numero)
     {
-        string inputName = "";
+        string inputName;
         List<int> valuesForPokemons = new List<int>();
-
-
-        //para poder llamar a 'GetValidatedNumber(1, n)'
-        int n = FabricaPokemon.DevolverTotal(); // debe ser la cantidad de pokemons que mostremos en la matriz
+        int n = FabricaPokemon.DevolverTotal();
 
         Console.WriteLine($"Enter the name of Player {numero}:");
         inputName = Console.ReadLine();
 
-        // if (g == 1)
+        ImpresoraDeTexto.mostrarListaPokemons(inputName);
+
+        for (int j = 0; j < 6; j++)
         {
-            ImpresoraDeTexto.mostrarListaPokemons(inputName);
-            for (int j = 0; j < 6; j++)
+            ImpresoraDeTexto.selectYourPokemon();
+
+            if (int.TryParse(Console.ReadLine(), out int numberOfPokemonSelected))
             {
-                ImpresoraDeTexto.selectYourPokemon();
                 try
                 {
-                    // este metodo recibe y verifica el valor ingresado
-                    int numberOfPokemonSelected = Convert.ToInt32(Console.ReadLine());
-                    // numberOfPokemonSelected = Calculator.GetValidatedNumber(1, n, numberOfPokemonSelected);
+                    numberOfPokemonSelected = Calculator.GetValidatedNumber(1, n, numberOfPokemonSelected);
                     valuesForPokemons.Add(numberOfPokemonSelected);
-                }
-                catch (FormatException)
-                {
-                    Console.WriteLine("Invalid format. Please enter a valid integer.");
-                    j--;
                 }
                 catch (ArgumentOutOfRangeException)
                 {
@@ -82,13 +80,38 @@ public static class Fachada
                     j--;
                 }
             }
-
-            List<IPokemon> PokemonsCreados = FabricaPokemon.InstanciarPokes(valuesForPokemons);
-            ImpresoraDeTexto.EligePokemonInicial(PokemonsCreados);
-            Console.WriteLine("Elige el pokemon inicial// no es definitivo.");
-            int eleccion = Convert.ToInt32(Console.ReadLine());
-            IPlayer player = new Player(inputName, PokemonsCreados, /*TODO revise this attribute*/ eleccion);
-            return player;
+            else
+            {
+                Console.WriteLine("Invalid format. Please enter a valid integer.");
+                j--;
+            }
         }
+
+        List<IPokemon> PokemonsCreados = FabricaPokemon.InstanciarPokes(valuesForPokemons);
+        ImpresoraDeTexto.EligePokemonInicial(PokemonsCreados);
+
+        int eleccion;
+        while (true)
+        {
+            Console.WriteLine("Choose your starting Pokémon:");
+            if (int.TryParse(Console.ReadLine(), out eleccion))
+            {
+                try
+                {
+                    eleccion = Calculator.GetValidatedNumber(1, PokemonsCreados.Count, eleccion);
+                    break;
+                }
+                catch (ArgumentOutOfRangeException)
+                {
+                    Console.WriteLine("Selection out of range. Please choose a valid Pokémon.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. Please enter a valid integer.");
+            }
+        }
+
+        return new Player(inputName, PokemonsCreados, eleccion);
     }
 }
